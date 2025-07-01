57a0620ba4dbf7c4f9dce4f86c3707564ad60afe
mikes987 <misk@posteo.de>
2025-07-01 12:51:13 -0400

    Finished Task 3.3

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -16,8 +16,7 @@ def get_request(endpoint, **kwargs):
     if(kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
-
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint# + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
