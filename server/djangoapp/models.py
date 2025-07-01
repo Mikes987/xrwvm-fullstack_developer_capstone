b6e353af370dfb6db35deb29f1cc7776856c2242
mikes987 <misk@posteo.de>
2025-07-01 04:40:27 -0400

    Finished Task 3.2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,34 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
+from django.db import models
 # from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return "Name: " + self.name
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    # dealer_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    car_type = models.CharField(max_length=100, choices=CAR_TYPES)
+    year = models.IntegerField(validators=[MinValueValidator(1990), MaxValueValidator(2025)])
+
+    def __str__(self):
+        return "Carmodel: " + self.name + ", CarMake: " + self.car_make.name
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
