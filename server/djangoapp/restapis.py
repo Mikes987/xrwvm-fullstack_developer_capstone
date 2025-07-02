af1eb1df9f66083b45deee5f5253d91dc775c5d1
Michael Skrzypczak <62150364+Mikes987@users.noreply.github.com>
2025-07-02 11:21:19 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -28,6 +28,7 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred: " + str(e))
 
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
