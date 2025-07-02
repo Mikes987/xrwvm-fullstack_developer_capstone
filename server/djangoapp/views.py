00e9ada5d2ef4a32196a4b696721a0856ad2e31a
Michael Skrzypczak <62150364+Mikes987@users.noreply.github.com>
2025-07-02 11:17:48 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -108,7 +108,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers":dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 def get_dealer_details(request, dealer_id):
